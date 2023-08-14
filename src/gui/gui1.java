69bc6449c30e8cefe316cb37eb329f90a4e9bef0
unknown <jlassi.haroun@esprit.tn>
2023-08-14 03:37:15 +0100

    designUI

--- src/gui/gui1.java
+++ src/gui/gui1.java
@@ -8,37 +8,49 @@ package gui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author msi
  */
 public class gui1 extends Application {
+            double x, y = 0;       
+
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+       // try {
+            Parent root = FXMLLoader.load(getClass().getResource("sgui1.fxml"));
+           stage.initStyle(StageStyle.TRANSPARENT);
+            /*appele aux fichiet qui regroupe tout lse element graphique de notre interface */
+            Scene scene = new Scene(root);
+            scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+            //stage.setTitle("Inscription");
+                //stage.initStyle(StageStyle.UNDECORATED);
+            root.setOnMousePressed(evt -> {
+
+                x = evt.getSceneX();
+                y = evt.getSceneY();
+
+            });
+            root.setOnMouseDragged(evt -> {
+                stage.setX(evt.getScreenX()- x);
+                stage.setY(evt.getScreenY()- y);
+
+            });
+		 
+            stage.setScene(scene);
+            stage.show();
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
     }
 
     /**
